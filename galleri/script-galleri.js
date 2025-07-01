71679f07c35468909bc0baf94f44de2126a86998
Laykajenta <marianne.odegaard@outlook.com>
2025-07-01 08:12:00 +0200

    lagt til script for loader

--- galleri/script-galleri.js
+++ galleri/script-galleri.js
@@ -1,3 +1,18 @@
+//spinner for loading
+// function to generate random color
+function getRandomColor() { 
+    return '#' + Math.random().toString(16).slice(-6); 
+}
+// function set random colors to the squares
+function setRandomColors() {
+    document.querySelector('.color1').style.backgroundColor = getRandomColor();
+    document.querySelector('.color2').style.backgroundColor = getRandomColor();
+    document.querySelector('.color3').style.backgroundColor = getRandomColor();
+}
+// initlialize with random colors
+setRandomColors();
+// continue to change colors every 2 seconds
+setInterval(setRandomColors, 2000);
 // allow for selceting of images to trigger modal view
 const items = document.querySelectorAll('.item');
 items.forEach(item => {
