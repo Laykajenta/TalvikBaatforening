beede82eb07c7974bf853c385a0680dd5b176d20
Laykajenta <96973776+Laykajenta@users.noreply.github.com>
2026-07-05 06:53:52 +0200

    oppdatert fargeskala og lagt  til global.css

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from './App.jsx'
+import "./styles/global.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
